dee9d9c99ca7139a08c570be6bf0097501f4875b
Ozan <ozanicli62@outlook.com>
2026-04-20 23:32:49 +0300

    finish url api

--- frontend/dashboard/src/supabase.js
+++ frontend/dashboard/src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://lfqkmlvrpeqnshgeoxdj.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxmcWttbHZycGVxbnNoZ2VveGRqIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3MzMzNTA5MSwiZXhwIjoyMDg4OTExMDkxfQ.pufEOq3m3NIWWfykBrUDY-tz1iLwReQ25aFfwrI7iRo';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxmcWttbHZycGVxbnNoZ2VveGRqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMzMzUwOTEsImV4cCI6MjA4ODkxMTA5MX0.aayWZufwRx159CR9soQoAbViWDzbiVxCxlVYPSuGXNI';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
